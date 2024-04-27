dddb66302755ee809f2c9de5e996d712776fa849
pgawde1 <163200353+pgawde1@users.noreply.github.com>
2024-04-27 16:47:57 -0400

    main

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ int main() {
 	GameEngine engine;
 	engine.initializeGame();
 	engine.printField();
-	
-	getchar();
+	engine.intro();
 
+	while(engine.remainingVeggies()!=0){
+		cout<<engine.remainingVeggies()<<" Veggies remaining. Current Score: "<<engine.getScore()<<endl;
+		engine.printField();
+		engine.moveRabbits();
+		engine.moveCaptain();
+		engine.timerTick();
+
+	}
+	engine.gameOver();
 	return 0;
 
 }
